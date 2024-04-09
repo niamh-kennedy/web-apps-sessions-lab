56d2b588ff713324be8bb44d8edbbdc62cf9ac1a
n14mh <b00158362@mytudublin.ie>
2024-04-09 22:45:16 +0100

    Part 5 - Logout

--- public/index.php
+++ public/index.php
@@ -19,9 +19,8 @@
 
         <div class="mainarea">
             <h1>Status: You are logged in <?php echo $_SESSION['Username']; ?></h1>
-            <p class="lead">This is where we will put the logout button</p>
 
-            <form action="" method="post" name="Logout_Form" class="form-signin">
+            <form action="logout.php" method="post" name="Logout_Form" class="form-signin">
                 <button name="Submit" value="Logout" class="button" type="submit">Log out</button>
             </form>
         </div>
---
+++ public/logout.php
@@ -0,0 +1,6 @@
+<?php
+	require_once '../src/session.php';
+	$session = new session();
+	$session->forgetSession();
+?>
+	
