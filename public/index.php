0303fbe407fce532d6dfa243ea8041fa50551a5d
n14mh <b00158362@mytudublin.ie>
2024-04-17 00:44:27 +0100

    Final - Fixed an issue with login verification.  Wasn't checking both username and password against database.

---
+++ public/index.php
@@ -0,0 +1,30 @@
+<?php require_once '../templates/header.php';?>
+
+<title>Home page</title>
+
+
+<body>
+<div class="container">
+    <div class="header clearfix">
+        <?php require_once '../templates/navBar.php';?>
+
+        <h3 class="text-muted">PHP Login exercise - Contacts page</h3>
+    </div>
+
+    <div class="mainarea">
+        <h1>Status: You are logged in <?php echo $_SESSION['Username']; ?></h1>
+
+        <form action="logout.php" method="post" name="Logout_Form" class="form-signin">
+            <button name="Submit" value="Logout" class="button" type="submit">Log out</button>
+        </form>
+    </div>
+
+    <div class="row marketing">
+        <div>
+            <h4>Contacts page</h4>
+            <p>Some content goes here. Some content goes here. Some content goes here. Some content goes here. Some content goes here. Some content goes here. Some content goes here. Some content goes here. Some content goes here. Some content goes here. Some content goes here. Some content goes here. Some content goes here. Some content goes here. Some content goes here. Some content goes here. Some content goes here. Some content goes here. Some content goes here. Some content goes here. Some content goes here. </p>
+
+        </div>
+
+
+<?php require_once '../templates/footer.php';?>
